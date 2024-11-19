7a65d878fd9428a7e95f640dcd3dd0d04e178fcb
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2024-11-19 19:48:44 +0530

    Added some more configs to quick-scope

--- lua/custom/plugins/motion.lua
+++ lua/custom/plugins/motion.lua
@@ -22,6 +22,8 @@ return {
       vim.cmd [[
       let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
       let g:qs_max_chars = 150
+      let g:qs_lazy_highlight = 1
+      let g:qs_buftype_blacklist = ['terminal', 'nofile']
 
     " Highlight groups Red and Blue
       autocmd ColorScheme * highlight QuickScopeSecondary guifg=#00ff00 gui=underline ctermfg=21 cterm=underline
