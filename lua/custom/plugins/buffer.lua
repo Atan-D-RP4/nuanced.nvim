65db61e1824610902e112df66ceb4830e0fb5ae9
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2024-12-06 12:28:36 +0530

    Turned Snipe into discount harpoon and got clever with settings the keymaps

--- lua/custom/plugins/buffer.lua
+++ lua/custom/plugins/buffer.lua
@@ -17,17 +17,45 @@ return {
   },
 
   {
-    -- Bufferswitcher
+    -- Buffer-Switcher
     'leath-dub/snipe.nvim',
-    keys = {
+    keys = vim.list_extend(
       {
-        'E',
-        function()
-          require('snipe').open_buffer_menu()
-        end,
-        desc = 'Open Snipe buffer menu',
+        {
+          '<leader>ee',
+          function()
+            require('snipe').open_buffer_menu()
+          end,
+          desc = 'Open Snipe buffer menu',
+        },
       },
-    },
+      vim.tbl_map(function(i)
+        return {
+          string.format('<leader>e%d', i),
+          function()
+            local Snipe = require 'snipe'
+            local cmd = Snipe.config.sort == 'last' and 'ls t' or 'ls'
+            local items = require('snipe.buffer').get_buffers(cmd)
+
+            if Snipe.config.ui.text_align == 'file-first' then
+              items = Snipe.file_first_format(items)
+            end
+
+            if i > #items or i == 0 then
+              vim.notify('Buffer index out of range', vim.log.levels.ERROR)
+              return
+            end
+
+            -- Jump to the nth buffer
+            Snipe.global_menu:close()
+            Snipe.global_menu.opened_from_wid = Snipe.global_menu:open_over()
+            vim.api.nvim_set_current_win(Snipe.global_menu.opened_from_wid)
+            vim.api.nvim_set_current_buf(items[i].id)
+          end,
+          desc = string.format('Jump to buffer %d', i),
+        }
+      end, { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+    ),
     opts = {
       hints = {
         dictionary = '1234567890',
