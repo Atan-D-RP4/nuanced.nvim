7480badfddbe9ae4f3233a427dc601f1861e9dfb
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-01-06 06:40:59 +0530

    More autocmds inspired by LazyVim and group name aggregation by nuance prefix

--- lua/nuance/core/autocmds.lua
+++ lua/nuance/core/autocmds.lua
@@ -1,12 +1,16 @@
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup('nuance-' .. name, { clear = true })
+end
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('nuance-highlight-yank', { clear = true }),
+  group = augroup 'highlight-yank',
   callback = function()
     local clip = '/mnt/c/Windows/System32/clip.exe'
     if vim.fn.executable(clip) == 1 then
@@ -23,7 +27,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- -- Trigger an Autocommand everytime the buffer list changes
 -- vim.api.nvim_create_autocmd({'BufAdd', 'BufDelete'}, {
 --   desc = 'Trigger an Autocommand everytime the buffer list changes',
---   group = vim.api.nvim_create_augroup('nuance-buffer-list-changes', { clear = true }),
+--   group = augroup('buffer-list-changes'),
 --   callback = function()
 --     if vim.g.listed_bufs == nil then
 --       vim.g.listed_bufs = {}
@@ -57,7 +61,7 @@ vim.api.nvim_create_autocmd({ 'RecordingEnter', 'RecordingLeave' }, {
 
 vim.api.nvim_create_autocmd('VimResized', {
   desc = 'Resize splits when resizing the window',
-  group = vim.api.nvim_create_augroup('nuance-resize-splits', { clear = true }),
+  group = augroup 'resize-splits',
   callback = function()
     vim.cmd 'wincmd ='
   end,
@@ -66,7 +70,7 @@ vim.api.nvim_create_autocmd('VimResized', {
 -- Clear trailing whitespace on save
 vim.api.nvim_create_autocmd('BufWritePre', {
   desc = 'Clear trailing whitespace on save',
-  group = vim.api.nvim_create_augroup('nuance-clear-trailing-whitespace', { clear = true }),
+  group = augroup 'clear-trailing-whitespace',
   callback = function()
     local save = vim.fn.winsaveview()
     vim.cmd [[keeppatterns %s/\s\+$//e]]
@@ -75,14 +79,20 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 })
 
 -- Close buffer if the terminal is closed
-vim.api.nvim_create_autocmd('TermClose', {
+vim.api.nvim_create_autocmd({ 'TermClose', 'TermOpen' }, {
   pattern = '*',
-  callback = function()
-    vim.schedule(function()
-      if (vim.bo.buftype == 'terminal' or vim.bo.filetype == 'lua') and vim.v.shell_error == 0 then
-        vim.cmd('bdelete! ' .. vim.fn.expand '<abuf>')
-      end
-    end)
+  callback = function(ev)
+    if ev.event == 'TermClose' then
+      vim.schedule(function()
+        if (vim.bo.buftype == 'terminal' or vim.bo.filetype == 'lua') and vim.v.shell_error == 0 then
+          vim.cmd('bdelete! ' .. vim.fn.expand '<abuf>')
+        end
+      end)
+    elseif ev.event == 'TermOpen' then
+      vim.opt.relativenumber = false
+      vim.opt.spell = false
+      vim.opt.number = false
+    end
   end,
 })
 
@@ -121,10 +131,10 @@ vim.api.nvim_create_autocmd('BufEnter', { command = [[set formatoptions-=cro]] }
 -- NOTE: Originally tried to put this in FileType event autocmd but it is apparently
 -- too early for `set modifiable` to take effect
 vim.api.nvim_create_autocmd('BufWinEnter', {
-  group = vim.api.nvim_create_augroup('EditQuickfix', { clear = true }),
+  group = vim.api.nvim_create_augroup('edit-quickfix', { clear = true }),
   desc = 'allow updating quickfix window',
   pattern = 'quickfix',
-  callback = function(ctx)
+  callback = function()
     vim.bo.modifiable = true
     -- :vimgrep's quickfix window display format now includes start and end column (in vim and nvim) so adding 2nd format to match that
     vim.bo.errorformat = '%f|%l col %c| %m,%f|%l col %c-%k| %m'
@@ -134,6 +144,52 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
       '<Cmd>cgetbuffer|set nomodified|echo "quickfix/location list updated"<CR>',
       { buffer = true, desc = 'Update quickfix/location list with changes made in quickfix window' }
     )
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('close-with-q', { clear = true }),
+  pattern = {
+    'PlenaryTestPopup',
+    'checkhealth',
+    'dbout',
+    'gitsigns-blame',
+    'grug-far',
+    'help',
+    'lspinfo',
+    'neotest-output',
+    'neotest-output-panel',
+    'neotest-summary',
+    'notify',
+    'fugitive',
+    'qf',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set('n', 'q', function()
+        vim.cmd 'close'
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = 'Quit buffer',
+      })
+    end)
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = vim.api.nvim_create_augroup('auto-create-dir', { clear = true }),
+  callback = function(event)
+    if event.match:match '^%w%w+:[\\/][\\/]' then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
 })
 
