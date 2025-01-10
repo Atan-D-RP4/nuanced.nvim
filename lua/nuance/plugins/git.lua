d0540797aed1a7ad791868e616ae57758dcdb1d2
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-01-10 18:40:53 +0530

    Modularize plugin specs

--- lua/nuance/plugins/git.lua
+++ lua/nuance/plugins/git.lua
@@ -1,93 +1,111 @@
-local M = {
-  {
-    'tpope/vim-fugitive',
-    cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gcommit', 'Gdiff' },
-    keys = {
-      { '<leader>gg', '<cmd>Git<CR>', desc = '[G]it', mode = 'n' },
-    },
-  },
-
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map('n', ']c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { ']c', bang = true }
-          else
-            gitsigns.nav_hunk 'next'
-          end
-        end, { desc = 'Jump to next git [c]hange' })
-
-        map('n', '[c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { '[c', bang = true }
-          else
-            gitsigns.nav_hunk 'prev'
-          end
-        end, { desc = 'Jump to previous git [c]hange' })
-
-        -- Actions
-        -- visual mode
-        map('v', 'gs', function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', 'gr', function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
-        -- normal mode
-        map('n', '<leader>ga', gitsigns.stage_hunk, { desc = '[G]it [a]dd hunk' })
-        map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[G]it [r]eset hunk' })
-        map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[G]it [S]tage buffer' })
-        map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = '[G]it [u]ndo stage hunk' })
-        map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[G]it [R]eset buffer' })
-        map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[G]it [p]review hunk' })
-        map('n', '<leader>gb', gitsigns.blame_line, { desc = '[G]it [b]lame line' })
-        map('n', '<leader>gd', gitsigns.diffthis, { desc = '[G]it [d]iff against index' })
-        map('n', '<leader>gD', function()
-          gitsigns.diffthis '@'
-        end, { desc = 'git [D]iff against last commit' })
-        -- Toggles
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-        map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
-        local state = 1
-        map('n', '<leader>tg', function()
-          if state == 1 then
-            vim.notify('Git signs disabled', vim.log.levels.WARN, { title = 'Gitsigns' })
-            state = 0
-          else
-            vim.notify('Git signs enabled', vim.log.levels.INFO, { title = 'Gitsigns' })
-            state = 1
-          end
-          gitsigns.toggle_signs()
-        end, { desc = '[T]oggle [g]it signs' })
-      end,
-    },
-  },
-
-  {
-    'sindrets/diffview.nvim',
-    enabled = false,
-    cmd = 'DiffviewOpen',
-    opts = {
-      use_icons = false, -- Requires nvim-web-devicons
-    },
+local gitcore = {
+  'tpope/vim-fugitive',
+  cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gcommit', 'Gdiff' },
+  keys = {
+    { '<leader>gg', '<cmd>Git<CR>', desc = '[G]it', mode = 'n' },
   },
 }
+
+local gitsigns = { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  opts = {},
+}
+
+local gitdiffview = {
+  'sindrets/diffview.nvim',
+  enabled = false,
+  cmd = 'DiffviewOpen',
+  opts = {
+    use_icons = false, -- Requires nvim-web-devicons
+  },
+}
+
+-- TODO: Implement integration with fzf-lua
+local gitworktree = {
+  'ThePrimeagen/git-worktree.nvim',
+  event = 'VeryLazy',
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- 'ibhagwan/fzf-lua',
+  },
+}
+
+local M = {
+  gitcore,
+  gitsigns,
+  -- gitdiffview,
+  -- gitworktree,
+}
+
+gitsigns.signs = {
+  add = { text = '+' },
+  change = { text = '~' },
+  delete = { text = '_' },
+  topdelete = { text = '‾' },
+  changedelete = { text = '~' },
+}
+
+gitsigns.opts.on_attach = function(bufnr)
+  local signs = require 'gitsigns'
+
+  local function map(mode, l, r, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
+    vim.keymap.set(mode, l, r, opts)
+  end
+
+  -- Navigation
+  map('n', ']c', function()
+    if vim.wo.diff then
+      vim.cmd.normal { ']c', bang = true }
+    else
+      signs.nav_hunk 'next'
+    end
+  end, { desc = 'Jump to next git [c]hange' })
+
+  map('n', '[c', function()
+    if vim.wo.diff then
+      vim.cmd.normal { '[c', bang = true }
+    else
+      signs.nav_hunk 'prev'
+    end
+  end, { desc = 'Jump to previous git [c]hange' })
+
+  -- Actions
+  -- visual mode
+  map('v', 'gs', function()
+    signs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+  end, { desc = 'stage git hunk' })
+  map('v', 'gr', function()
+    signs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+  end, { desc = 'reset git hunk' })
+  -- normal mode
+  map('n', '<leader>ga', signs.stage_hunk, { desc = '[G]it [a]dd hunk' })
+  map('n', '<leader>gr', signs.reset_hunk, { desc = '[G]it [r]eset hunk' })
+  map('n', '<leader>gS', signs.stage_buffer, { desc = '[G]it [S]tage buffer' })
+  map('n', '<leader>gu', signs.undo_stage_hunk, { desc = '[G]it [u]ndo stage hunk' })
+  map('n', '<leader>gR', signs.reset_buffer, { desc = '[G]it [R]eset buffer' })
+  map('n', '<leader>gp', signs.preview_hunk, { desc = '[G]it [p]review hunk' })
+  map('n', '<leader>gb', signs.blame_line, { desc = '[G]it [b]lame line' })
+  map('n', '<leader>gd', signs.diffthis, { desc = '[G]it [d]iff against index' })
+  map('n', '<leader>gD', function()
+    signs.diffthis '@'
+  end, { desc = 'git [D]iff against last commit' })
+  -- Toggles
+  map('n', '<leader>tb', signs.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+  map('n', '<leader>tD', signs.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
+  local state = 1
+  map('n', '<leader>tg', function()
+    if state == 1 then
+      vim.notify('Git signs disabled', vim.log.levels.WARN, { title = 'Gitsigns' })
+      state = 0
+    else
+      vim.notify('Git signs enabled', vim.log.levels.INFO, { title = 'Gitsigns' })
+      state = 1
+    end
+    signs.toggle_signs()
+  end, { desc = '[T]oggle [g]it signs' })
+end
+
 return M
--- lua/nuance/plugins/oil.lua
+++ lua/nuance/plugins/oil.lua
@@ -1,184 +1,153 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    version = '*',
-    cmd = 'Oil',
+local M = {
+  'stevearc/oil.nvim',
+  version = '*',
+  cmd = 'Oil',
 
-    keys = { { '<leader>o', '<cmd>lua require("oil").toggle_float()<CR>', mode = 'n', desc = 'Open Oil Window' } },
+  keys = { { '<leader>o', '<cmd>lua require("oil").toggle_float()<CR>', mode = 'n', desc = 'Open Oil Window' } },
 
-    init = function()
-      ---@diagnostic disable-next-line: param-type-mismatch
-      if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
-        require('lazy').load { plugins = { 'oil.nvim' } }
-        vim.cmd 'bd' -- Close the initial buffer
-        vim.cmd('Oil ' .. vim.fn.argv(0))
-      end
-    end,
+  init = function()
+    ---@diagnostic disable-next-line: param-type-mismatch
+    if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      require('lazy').load { plugins = { 'oil.nvim' } }
+      vim.cmd 'bd' -- Close the initial buffer
+      vim.cmd('Oil ' .. vim.fn.argv(0))
+    end
+  end,
+}
 
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-      default_file_explorer = true,
+---@module 'oil'
+---@class oil.SetupOpts
+M.opts = {
+  default_file_explorer = true,
+  use_default_keymaps = true,
+  skip_confirm_for_simple_edits = true,
+  delete_to_trash = false,
+  prompt_save_on_select_new_entry = true,
+  ssh = { border = 'rounded' },
+  keymaps_help = { border = 'rounded' },
+  constrain_cursor = 'editable',
+  watch_for_changes = true,
+  columns = { 'icon', 'size', 'modified' },
+  cleanup_delay_ms = 2000,
+  extra_scp_args = {},
+}
 
-      -- columns = {
-      --   'icon',
-      -- },
+M.opts.buf_options = {
+  buflisted = false,
+  bufhidden = 'hide',
+}
 
-      buf_options = {
-        buflisted = false,
-        bufhidden = 'hide',
-      },
+M.opts.win_options = {
+  wrap = false,
+  signcolumn = 'yes:2',
+  cursorcolumn = false,
+  foldcolumn = '0',
+  spell = false,
+  list = false,
+  conceallevel = 3,
+  concealcursor = 'nvic',
+}
 
-      win_options = {
-        wrap = false,
-        signcolumn = 'yes:2',
-        cursorcolumn = false,
-        foldcolumn = '0',
-        spell = false,
-        list = false,
-        conceallevel = 3,
-        concealcursor = 'nvic',
-      },
+M.opts.lsp_file_methods = {
+  enabled = true,
+  timeout_ms = 1000,
+  autosave_changes = false,
+}
 
-      delete_to_trash = false,
+M.opts.keymaps = {
+  ['g?'] = 'actions.show_help',
+  ['l'] = 'actions.select',
+  ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+  ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+  ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+  ['<C-p>'] = 'actions.preview',
+  ['q'] = 'actions.close',
+  ['<C-l>'] = 'actions.refresh',
+  ['<C-x>'] = '',
+  ['-'] = 'actions.parent',
+  ['_'] = 'actions.open_cwd',
+  ['`'] = 'actions.cd',
+  ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory', mode = 'n' },
+  ['gs'] = 'actions.change_sort',
+  ['gx'] = 'actions.open_external',
+  ['g.'] = 'actions.toggle_hidden',
+  ['g\\'] = 'actions.toggle_trash',
+  ['<Right>'] = 'actions.select',
+}
 
-      skip_confirm_for_simple_edits = false,
+M.opts.view_options = {
+  show_hidden = false,
+  natural_order = true,
 
-      prompt_save_on_select_new_entry = true,
+  is_hidden_file = function(name, _) -- function(name, bufnr)
+    return vim.startswith(name, '.')
+  end,
 
-      cleanup_delay_ms = 2000,
-      lsp_file_methods = {
+  is_always_hidden = function(_, _) -- function(name, bufnr)
+    return false
+  end,
 
-        enabled = true,
-
-        timeout_ms = 1000,
-
-        autosave_changes = false,
-      },
-
-      constrain_cursor = 'editable',
-
-      watch_for_changes = false,
-
-      keymaps = {
-        ['g?'] = 'actions.show_help',
-        ['l'] = 'actions.select',
-        ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-        ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
-        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
-        ['<C-p>'] = 'actions.preview',
-        ['q'] = 'actions.close',
-        ['<C-l>'] = 'actions.refresh',
-        ['<C-x>'] = '',
-        ['-'] = 'actions.parent',
-        ['_'] = 'actions.open_cwd',
-        ['`'] = 'actions.cd',
-        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory', mode = 'n' },
-        ['gs'] = 'actions.change_sort',
-        ['gx'] = 'actions.open_external',
-        ['g.'] = 'actions.toggle_hidden',
-        ['g\\'] = 'actions.toggle_trash',
-        ['<Right>'] = 'actions.select',
-      },
-
-      use_default_keymaps = true,
-      view_options = {
-
-        show_hidden = false,
-
-        is_hidden_file = function(name, bufnr)
-          return vim.startswith(name, '.')
-        end,
-
-        natural_order = true,
-
-        is_always_hidden = function(name, bufnr)
-          return false
-        end,
-
-        case_insensitive = false,
-        sort = {
-          { 'mtime', 'desc' },
-          { 'type', 'asc' },
-          { 'name', 'asc' },
-        },
-      },
-
-      extra_scp_args = {},
-
-      git = {
-        add = function(path)
-          return false
-        end,
-        mv = function(src_path, dest_path)
-          return false
-        end,
-        rm = function(path)
-          return false
-        end,
-      },
-
-      float = {
-
-        padding = 2,
-        max_width = 0,
-        max_height = 0,
-        border = 'rounded',
-        win_options = {
-          winblend = 0,
-        },
-
-        get_win_title = nil,
-
-        preview_split = 'auto',
-
-        override = function(conf)
-          return conf
-        end,
-      },
-
-      preview = {
-
-        max_width = 0.9,
-
-        min_width = { 40, 0.4 },
-
-        width = nil,
-
-        max_height = 0.9,
-
-        min_height = { 5, 0.1 },
-
-        height = nil,
-        border = 'rounded',
-        win_options = {
-          winblend = 0,
-        },
-
-        update_on_cursor_moved = true,
-      },
-
-      progress = {
-        max_width = 0.9,
-        min_width = { 40, 0.4 },
-        width = nil,
-        max_height = { 10, 0.9 },
-        min_height = { 5, 0.1 },
-        height = nil,
-        border = 'rounded',
-        minimized_border = 'none',
-        win_options = {
-          winblend = 0,
-        },
-      },
-
-      ssh = {
-        border = 'rounded',
-      },
-
-      keymaps_help = {
-        border = 'rounded',
-      },
-    },
+  case_insensitive = false,
+  sort = {
+    { 'mtime', 'desc' },
+    { 'type', 'asc' },
+    { 'name', 'asc' },
   },
 }
+
+M.opts.git = {
+  add = function(path)
+    return false
+  end,
+  mv = function(src_path, dest_path)
+    return false
+  end,
+  rm = function(path)
+    return false
+  end,
+}
+
+M.opts.float = {
+  padding = 5,
+  max_width = 0,
+  max_height = 0,
+  border = 'rounded',
+
+  win_options = {
+    winblend = 0,
+  },
+
+  get_win_title = nil,
+  preview_split = 'auto',
+
+  override = function(conf)
+    return conf
+  end,
+}
+
+---@class M.opts
+M.opts.preview = {
+  max_width = 0.9,
+  min_width = { 40, 0.4 },
+  width = nil,
+  max_height = 0.9,
+  min_height = { 5, 0.1 },
+  height = nil,
+  border = 'rounded',
+  update_on_cursor_moved = true,
+  win_options = { winblend = 0 },
+}
+
+M.opts.progress = {
+  max_width = 0.9,
+  min_width = { 40, 0.4 },
+  width = nil,
+  max_height = { 10, 0.9 },
+  min_height = { 5, 0.1 },
+  height = nil,
+  border = 'rounded',
+  minimized_border = 'none',
+  win_options = { winblend = 0 },
+}
+
+return M
