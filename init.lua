5fb4d4c4d67a7bccf08a9e5344a4ece6f0a94e73
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-01-20 21:06:00 +0530

    Plugins added and removed. Separated term.lua into it's own plugin repo
    CHANGELOG:
    - Added dropbar.nvim spec into buffer.lua and an example nvim-navic
      spec in lspconfig.lua.
    - Fixed fzf.lua config. Added config fn to correctly setup user-opts.
    - Figured out how lang-servers should be setup & formatted lspconfig.lua.
    - Change mini.surround event from VeryLazy to Buf[Read|NewFile] events.
    - Lazyloading mini.ai and added in it's default conf in motion.lua.
    - Changed flash.nvim labels and added better desc for flash keybinds.
    - Modified oil.nvim config, <CR> is no-op now, '=' is used to sync to
      filesystem. Updated opts also. Explore oil.lua for details.
    - Commented out a bunch of keybinds that came with the default
      snacks.nvim config
    - Re-enabled nvim-treesitter-textobjects.
    - Re-factored treesitter.lua
    - Changed trouble.nvim keybinds to make better sense
    - Enabled noice.nvim spec
    - Added config for neovide

--- init.lua
+++ init.lua
@@ -1,16 +1,54 @@
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 -- vim.g.have_nerd_font = true
 
--- vim.loader.enable()
+-- Neovide settings
+if vim.g.neovide then
+	local cursor_vfx = { "railgun", "torpedo", "pixiedust", "sonicboom", "ripple", "wireframe" }
+	math.randomseed(os.time())
 
--- [[ Load the core configuration ]]
-require 'nuance.core'
+	vim.g.neovide_cursor_vfx_mode = cursor_vfx[math.random(1, #cursor_vfx)]
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+	vim.keymap.set("n", "<F11>", function()
+		vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+	end, {})
 
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+	vim.g.neovide_scale_factor = 0.78
+	vim.g.neovide_floating_blur_amount_x = 10.0
+	vim.g.neovide_floating_blur_amount_y = 10.0
+	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_refresh_rate = 24 -- 144, 60 This is fun to play with
+	vim.g.neovide_refresh_rate_idle = 1
+	vim.g.neovide_confirm_quit = false
+	vim.g.neovide_no_idle = false
+	vim.g.neovide_floating_corner_radius = 0.5
+
+	vim.g.neovide_title_background_color =
+		string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+
+	vim.g.neovide_title_text_color = "pink"
+
+	vim.o.laststatus = 0
+	vim.o.cmdheight = 0
+	vim.o.guifont = "FantasqueSansM Nerd Font,:h20" -- Nerd fonts work well
+
+	vim.cmd([[
+terminal
+startinsert
+autocmd BufLeave term://* quit
+]])
+else
+	vim.loader.enable()
+
+	-- [[ Load the core configuration ]]
+	require("nuance.core")
+
+	-- [[ Install `lazy.nvim` plugin manager ]]
+	require("lazy-bootstrap")
+
+	-- [[ Configure and install plugins ]]
+	require("lazy-plugins")
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
