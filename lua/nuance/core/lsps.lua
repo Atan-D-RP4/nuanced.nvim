25d6af9116106c8a513cd248a693498c9d271b06
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-10-30 01:11:30 +0530

    CHANGELOG (NVIM v0.12.0-dev-1512+g29e093c8f2)
    -  My PR to nvim-lspconfig for codelens support on rust-analyzer was
       merged! So I don't need that code in my config anymore.

--- lua/nuance/core/lsps.lua
+++ lua/nuance/core/lsps.lua
@@ -381,39 +381,6 @@ return {
   rust_analyzer = {
     enabled = vim.fn.executable 'rust-analyzer' == 1,
 
-    before_init = function(_, _)
-      ---@param command table
-      vim.lsp.commands['rust-analyzer.runSingle'] = function(command)
-        local r = command.arguments[1]
-        local cmd = { 'cargo', unpack(r.args.cargoArgs) }
-        if r.args.executableArgs and #r.args.executableArgs > 0 then
-          vim.list_extend(cmd, { '--', unpack(r.args.executableArgs) })
-        end
-
-        local proc = vim.system(cmd, { cwd = r.args.cwd })
-
-        local result = proc:wait()
-
-        if result.code == 0 then
-          vim.notify(result.stdout, vim.log.levels.INFO)
-        else
-          vim.notify(result.stderr, vim.log.levels.ERROR)
-        end
-      end
-    end,
-
-    capabilities = {
-      experimental = {
-        commands = {
-          commands = {
-            'rust-analyzer.showReferences',
-            'rust-analyzer.runSingle',
-            'rust-analyzer.debugSingle',
-          },
-        },
-      },
-    },
-
     settings = {
       ['rust-analyzer'] = {
         cargo = {
